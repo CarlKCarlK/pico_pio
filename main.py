e4b39ead129df97ecfe0e1ed9de14698437052fb
Carl Kadie <carlk@msn.com>
2025-01-05 11:18:22 -0800

    add comments to sound

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ import time
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
 def sound():
     label("wait_for_nonzero")
-    pull(block)             # Wait for an initial value from the FIFO
-    mov(x, osr)             # Store the frequency in X
-    jmp(not_x, "wait_for_nonzero")       # If x is zero, jump to "top"
-    wrap_target()           # Start of the loop
-    set(pins, 1)            # Set the pin high
+    pull(block)                     # Wait for a delay value, keep it in osr.
+    mov(x, osr)                     # Copy the delay into x.
+    jmp(not_x, "wait_for_nonzero")  # If delay is zero, wait for a non-zero value.
+    wrap_target()                   # Start of the main loop.
+    set(pins, 1)                    # Set the buzzer to high voltage.
     label("high_voltage_loop")
     jmp(x_dec, "high_voltage_loop") # Delay
-    mov(x, osr)               # Reload the delay into Y
-    set(pins, 0)            # Set the pin low
+    set(pins, 0)                    # Set the buzzer to low voltage.
+    mov(x, osr)                     # Reload the delay into x.
     label("low_voltage_loop")
     jmp(x_dec, "low_voltage_loop")  # Delay
-    mov(x, osr)
-    pull(noblock)           # If no value, copy x into osr
-    jmp(not_x, "wait_for_nonzero")       # If x is zero, jump to "top"
-    wrap()                  # End of the loop
+    mov(x, osr)                     # set x, the default value for "pull(nonblock)"
+    pull(noblock)                   # Read a new delay value or use the default.
+    jmp(not_x, "wait_for_nonzero")  # If x is zero, wait for a non-zero value.
+    wrap()                          # End of main loop.
 
 def demo_sound():
     CLK_FREQ = 125_000_000  # 125 MHz
     BUZZER_PIN = 15 
-    print("Hello, world7!")
+    print("Hello, world8!")
     
     pio = rp2.PIO(0)
     pio.remove_program()
